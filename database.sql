7a9c8b27fdc5b3478db22026e11da48e8346b5dd
parthnparmar <23amtics300@gmail.com>
2026-06-27 12:14:45 +0530

    Add files via upload

---
+++ database.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE IF NOT EXISTS crop_disease_db CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE crop_disease_db;
+
+-- Tables will be auto-created by SQLAlchemy, but you can use this for manual setup
